eadc33422c2fc3d62d561fc82c3196c9cd212a9e
Kirill <kirill@192.168.0.6>
2022-11-30 10:19:40 +0300

    min fix

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const { renderHome } = require("../controllers/indexControllers");
 
 router.get("/", renderHome);
+// router.get("/login", renderHome);
 
 module.exports = router;
